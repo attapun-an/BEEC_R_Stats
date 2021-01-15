037a540b8c732225c196a9a6ba6b8ce7f09ecfc3
attapun-an <attapunanivat@gmail.com>
2021-01-15 18:52:51 +0700

    pun finished the tutorial

--- Scripts/BEEC_stats_tutorial.R
+++ Scripts/BEEC_stats_tutorial.R
@@ -9,5 +9,28 @@ rm(list=ls())
 library(ggplot2)
 
 # Loading Data ----
-limpetdata <- read.table(file.choose(),header=TRUE)
-limpetdata <- read.table('Data/limpet-data.txt', header = TRUE)
+limpetdata <- read.table(file.choose(),header=TRUE)  # Woow, Biology is cool~~~
+limpetdata <- read.table('Data/limpet-data.txt', header = TRUE) %>%   # But we're doing it the proper way (cause reproducibility) 
+dplyr::rename(shoreheight = height)
+
+head(limpetdata)
+str(limpetdata)
+  
+# Plotting ----
+ggplot(limpetdata, aes(x = shoreheight, y = width)) + geom_point()
+
+
+
+# Descriptive Stats ---- 
+summary(limpetdata)
+mean(limpetdata$width)
+sd(limpetdata$width)
+
+# Regressions ----
+
+model_width_shoreheight <- lm(width ~ shoreheight, data = limpetdata)
+
+summary(model_width_shoreheight)
+plot(model_width_shoreheight)
+       
+     
